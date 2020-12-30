a13c68debae9541d505d0660579a877629dd145f
oussama.elbouenani <oussama.elbouenani@student.vinci.be>
2020-12-30 17:19:15 +0100

    add - entire website

---
+++ views/accueil.php
@@ -0,0 +1,5 @@
+		<section id="contenu">
+			<h2>Accueil</h2>
+			<p>Bienvenue sur le site des bonnes nouvelles (encore une fois) afin de faire du DevOps.</p>
+			<p>Oussama EL BOUENANI.</p>
+		</section>
